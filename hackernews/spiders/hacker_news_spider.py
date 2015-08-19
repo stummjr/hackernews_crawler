f5d42be69f68f2ea8fd416a50aa4047ef3a3ea00
stummjr <stummjr@gmail.com>
2015-08-19 19:09:35 -0300

    using start_requests instead of start_urls

--- hackernews/spiders/hacker_news_spider.py
+++ hackernews/spiders/hacker_news_spider.py
@@ -5,13 +5,16 @@ from scrapy.linkextractors import LinkExtractor
 class HackerNewsSpider(scrapy.Spider):
     name = "hnews"
     allowed_domains = ["ycombinator.com"]
-    start_urls = ["http://news.ycombinator.com"]
+
+    def start_requests(self):
+        yield scrapy.Request("http://news.ycombinator.com", callback=self.parse)
 
     def parse(self, response):
         for sel in response.xpath("//tr[@class='athing']"):
             item = HackerNewsItem()
-            item['title'] = sel.xpath("./td[@class='title']/a/text()").extract()[0]
-            item['url'] = sel.xpath("./td[@class='title']/a/@href").extract()[0]
+            link_sel = sel.xpath("./td[@class='title']")
+            item['title'] = link_sel.xpath("./a/text()").extract()[0]
+            item['url'] = link_sel.xpath("./a/@href").extract()[0]
             details_sel = sel.xpath("./following-sibling::*[position()=1]/td[@class='subtext']")
             item['points'] = details_sel.xpath("./span[@class='score']/text()").extract_first()
             item['user_name'] = details_sel.xpath("./a[starts-with(@href, 'user')]/text()").extract_first()
