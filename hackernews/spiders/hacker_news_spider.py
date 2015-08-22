528331ba7064c89390bf7373cd4fe98310ed8aeb
stummjr <stummjr@gmail.com>
2015-08-21 23:03:18 -0300

    improved code organization

--- hackernews/spiders/hacker_news_spider.py
+++ hackernews/spiders/hacker_news_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-import urlparse
+from urlparse import urljoin
 from scrapy.linkextractors import LinkExtractor
 from hackernews.items import HackerNewsItem, CommentItem
 
@@ -9,40 +9,12 @@ class HackerNewsSpider(scrapy.Spider):
     allowed_domains = ["ycombinator.com"]
     start_urls = ["http://news.ycombinator.com"]
 
-    def extract_post(self, sel, response):
-        item = HackerNewsItem()
-        link_sel = sel.xpath("./td[@class='title']")
-        item['title'] = link_sel.xpath("./a/text()").extract_first()
-        item['url'] = link_sel.xpath("./a/@href").extract_first()
-        details_sel = sel.xpath(
-            "./following-sibling::*[position()=1]/td[@class='subtext']"
-        )
-        item['points'] = details_sel.xpath(
-            "./span[@class='score']/text()"
-        ).extract_first()
-        item['user_name'] = details_sel.xpath(
-            "./a[starts-with(@href, 'user')]/text()"
-        ).extract_first()
-        item['since'] = details_sel.xpath(
-            "./a[starts-with(@href, 'item')][contains(., 'ago')]/text()"
-        ).extract_first()
-        item['comments'] = details_sel.xpath(
-            "./a[starts-with(@href, 'item')][contains(., 'comment') or"
-            "contains(., 'discuss')]/text()"
-        ).extract_first()
-        item['comments_url'] = urlparse.urljoin(
-            response.url, details_sel.xpath(
-                "./a[starts-with(@href, 'item')]/@href"
-            ).extract_first()
-        )
-        return item
-
     def parse(self, response):
         for sel in response.xpath("//tr[@class='athing']"):
-            item = self.extract_post(sel, response)
+            item = self.extract_news_item(sel, response)
             yield item
             # ads have no comments
-            if item['comments_url'] != 'https://news.ycombinator.com/':
+            if item['comments_url'] is not None:
                 yield scrapy.Request(
                     item['comments_url'], callback=self.parse_comments
                 )
@@ -54,31 +26,59 @@ class HackerNewsSpider(scrapy.Spider):
 
     def parse_comments(self, response):
         items = []
+        # the first item is not a comment
         for athing_sel in response.xpath('//tr[@class="athing"]')[1:]:
-            item = CommentItem()
-            item['nesting_level'] = int(
+            comment_item = CommentItem()
+            comment_item['nesting_level'] = int(
                 athing_sel.xpath(
                     ".//td[@class='ind']/img/@width"
                 ).extract_first()
             )
-            item['text'] = "\n".join(
+            comment_item['text'] = "\n".join(
                 athing_sel.xpath(
                     ".//td[@class='default']//span[@class='comment']//text()"
                 ).extract()[1:-6]
             )
-            item['user_name'] = athing_sel.xpath(
+            comment_item['user_name'] = athing_sel.xpath(
                 ".//td[@class='default']//span[@class='comhead']/"
                 "a[starts-with(@href, 'user')]/text()"
             ).extract_first()
-            item['id_'] = athing_sel.xpath(
+            comment_item['id_'] = athing_sel.xpath(
                 ".//td[@class='default']//a[starts-with(@href, 'item')]/@href"
             ).extract_first()
-            item['hacker_news_item'] = response.url
-            items.append(item)
-
+            comment_item['hacker_news_item'] = response.url
+            items.append(comment_item)
         self.fill_parents(items)
         for item in items:
             yield item
+
+    def extract_news_item(self, sel, response):
+        news_item = HackerNewsItem()
+        link_sel = sel.xpath("./td[@class='title']")
+        news_item['title'] = link_sel.xpath("./a/text()").extract_first()
+        news_item['url'] = link_sel.xpath("./a/@href").extract_first()
+        details_sel = sel.xpath(
+            "./following-sibling::*[position()=1]/td[@class='subtext']"
+        )
+        news_item['points'] = details_sel.xpath(
+            "./span[@class='score']/text()"
+        ).extract_first()
+        news_item['user_name'] = details_sel.xpath(
+            "./a[starts-with(@href, 'user')]/text()"
+        ).extract_first()
+        news_item['since'] = details_sel.xpath(
+            "./a[starts-with(@href, 'item')][contains(., 'ago')]/text()"
+        ).extract_first()
+        news_item['comments'] = details_sel.xpath(
+            "./a[starts-with(@href, 'item')][contains(., 'comment') or"
+            "contains(., 'discuss')]/text()"
+        ).extract_first()
+        comments_path = details_sel.xpath(
+            "./a[starts-with(@href, 'item')]/@href"
+        ).extract_first()
+        news_item['comments_url'] = (comments_path and
+                                     urljoin(response.url, comments_path))
+        return news_item
 
     def get_parent_of(self, list_, current):
         for i in range(current, -1, -1):
